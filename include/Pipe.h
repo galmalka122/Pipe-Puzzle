ffd3de0352403522669be054ae0225b0d4e98cb4
DvirPerkin <Dvirperkin@gmail.com>
2021-05-26 17:07:59 +0300

    Update Pipe class
    Pipe intialization has updated.
    Added connection of pipes with a water source.

--- include/Pipe.h
+++ include/Pipe.h
@@ -2,22 +2,34 @@
 
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
+
 #include "Macros.h"
 #include "Textures.h"
+
+// A class that represent a pipe.
 
 class Pipe {
 public:
     Pipe(const char, const int, const sf::Vector2i &);
     void draw(sf::RenderWindow &);
+
     sf::Sprite getSprite() const;
+    char getType() const;
+
     void setPosition(sf::Vector2f);
     void rotateRight();
     void rotateLeft();
 
+    void connect();
+    void disconnect();
+
+    bool operator[](const int) const;
 private:
     char m_pipeType;
-    sf::Sprite m_sprite;
-    std::array<bool, SIDES> m_direction;
+    bool m_sourceConnect;
+    sf::Sprite m_pipeEmpty;
+    sf::Sprite m_pipeFull;
+    std::array<bool, SIDES> m_direction; // Indicates the pipe direction.
 
     void initDirection(const int);
     void pipeType(const char);
--- src/Pipe.cpp
+++ src/Pipe.cpp
@@ -1,84 +1,132 @@
 #include "Pipe.h"
 
-Pipe::Pipe(const char type, const int direction, const sf::Vector2i & ratio) : m_pipeType(type){
+Pipe::Pipe(const char type, const int direction, const sf::Vector2i & ratio)
+            : m_pipeType(type), m_sourceConnect(false){
 
     pipeType(type);
-    m_sprite.setOrigin(m_sprite.getGlobalBounds().width / 2,
-                       m_sprite.getGlobalBounds().height / 2);
-    m_sprite.setScale({BOARD_WIDTH / (m_sprite.getGlobalBounds().width * ratio.x),
-                       BOARD_HEIGHT / (m_sprite.getGlobalBounds().height * ratio.y)});
+
+    m_pipeEmpty.setOrigin(m_pipeEmpty.getGlobalBounds().width / 2,
+                          m_pipeEmpty.getGlobalBounds().height / 2);
+    m_pipeFull.setOrigin(m_pipeFull.getGlobalBounds().width / 2,
+                         m_pipeFull.getGlobalBounds().height / 2);
+
+    // Choose the smallest edge to avoid rectangles in the game board.
+    auto squareBound = std::min(BOARD_WIDTH / ratio.x, BOARD_HEIGHT / ratio.y);
+
+
+    m_pipeEmpty.setScale({squareBound / m_pipeEmpty.getGlobalBounds().width,
+                          squareBound / m_pipeEmpty.getGlobalBounds().height});
+    m_pipeFull.setScale({squareBound / m_pipeFull.getGlobalBounds().width,
+                         squareBound / m_pipeFull.getGlobalBounds().height});
+
     initDirection(direction);
 }
 //=============================================================================
+void Pipe::draw(sf::RenderWindow & window) {
+    m_sourceConnect ? window.draw(m_pipeFull) : window.draw(m_pipeEmpty);
+}
+//=============================================================================
+sf::Sprite Pipe::getSprite() const {
+    if(m_sourceConnect)
+        return m_pipeFull;
+    else
+        return m_pipeEmpty;
+}
+//=============================================================================
+char Pipe::getType() const {
+    return m_pipeType;
+}
+//=============================================================================
+void Pipe::setPosition(sf::Vector2f position) {
+    m_pipeEmpty.setPosition(position.x + m_pipeEmpty.getGlobalBounds().width / 2,
+                            position.y + m_pipeEmpty.getGlobalBounds().height / 2);
+
+    m_pipeFull.setPosition(position.x + m_pipeFull.getGlobalBounds().width / 2,
+                            position.y + m_pipeFull.getGlobalBounds().height / 2);
+}
+//=============================================================================
+void Pipe::rotateRight() {
+    m_pipeEmpty.rotate(-DEGREE);
+    m_pipeFull.rotate(-DEGREE);
+    std::swap(m_direction[UP], m_direction[LEFT]);
+    std::swap(m_direction[DOWN], m_direction[RIGHT]);
+    std::swap(m_direction[UP], m_direction[DOWN]);
+}
+//=============================================================================
+void Pipe::rotateLeft() {
+    m_pipeEmpty.rotate(DEGREE);
+    m_pipeFull.rotate(DEGREE);
+    std::swap(m_direction[UP], m_direction[RIGHT]);
+    std::swap(m_direction[DOWN], m_direction[LEFT]);
+    std::swap(m_direction[UP], m_direction[DOWN]);
+}
+//=============================================================================
+void Pipe::connect() {
+    m_sourceConnect = true;
+}
+//=============================================================================
+void Pipe::disconnect() {
+    if(m_pipeType != SOURCE)
+        m_sourceConnect = false;
+}
+//=============================================================================
+bool Pipe::operator[](const int side) const {
+    if(side < 0 or side > SIDES)
+        throw std::domain_error("Each pipe have " + std::to_string(SIDES) + " sides");
+
+    return m_direction[side];
+}
+//=============================================================================
+
+
+//----------------------------- Private Functions -----------------------------
 void Pipe::pipeType(const char type) {
     switch (type) {
         case SOURCE:
-            m_sprite = Textures::texturesObject().getSprite(SOURCE_F);
+            m_pipeEmpty = Textures::texturesObject().getSprite(SOURCE_F);
+            m_pipeFull = Textures::texturesObject().getSprite(SOURCE_F);
             m_direction = SOURCE_SINK_DIRECTION;
+            connect();
             break;
         case STRAIGHT:
-            m_sprite = Textures::texturesObject().getSprite(STRAIGHT_E);
+            m_pipeEmpty = Textures::texturesObject().getSprite(STRAIGHT_E);
+            m_pipeFull = Textures::texturesObject().getSprite(STRAIGHT_F);
             m_direction = STRAIGHT_DIRECTION;
             break;
         case CURVED:
-            m_sprite = Textures::texturesObject().getSprite(CURVED_E);
+            m_pipeEmpty = Textures::texturesObject().getSprite(CURVED_E);
+            m_pipeFull = Textures::texturesObject().getSprite(CURVED_F);
             m_direction = CURVED_DIRECTION;
             break;
         case T:
-            m_sprite = Textures::texturesObject().getSprite(T_E);
+            m_pipeEmpty = Textures::texturesObject().getSprite(T_E);
+            m_pipeFull = Textures::texturesObject().getSprite(T_F);
             m_direction = T_DIRECTION;
             break;
         case PLUS:
-            m_sprite = Textures::texturesObject().getSprite(PLUS_E);
+            m_pipeEmpty = Textures::texturesObject().getSprite(PLUS_E);
+            m_pipeFull = Textures::texturesObject().getSprite(PLUS_F);
             m_direction = PLUS_DIRECTION;
             break;
         case SINK:
-            m_sprite = Textures::texturesObject().getSprite(SINK_E);
+            m_pipeEmpty = Textures::texturesObject().getSprite(SINK_E);
+            m_pipeFull = Textures::texturesObject().getSprite(SINK_F);
             m_direction = SOURCE_SINK_DIRECTION;
             break;
         default:
-            throw std::domain_error("");
+            throw std::domain_error("No such pipe");
     }
 }
 //=============================================================================
 void Pipe::initDirection(const int direction) {
 
     if(direction % DEGREE != 0)
-        throw std::domain_error("");
+        throw std::domain_error("Degree must divided by 90");
 
-    m_sprite.rotate(direction);
-}
-//=============================================================================
-void Pipe::draw(sf::RenderWindow & window) {
-    window.draw(m_sprite);
-}
-//=============================================================================
-sf::Sprite Pipe::getSprite() const {
-    return m_sprite;
-}
-//=============================================================================
-void Pipe::setPosition(sf::Vector2f position) {
-    m_sprite.setPosition(position.x + m_sprite.getGlobalBounds().width/2,
-                         position.y + m_sprite.getGlobalBounds().height/2);
-}
-//=============================================================================
-void Pipe::rotateRight() {
-    if(m_pipeType != SINK) {
-        m_sprite.rotate(DEGREE);
+    //Calculates the number of rotation in range 0 to 3.
+    auto numOfRotate = (direction/DEGREE) % SIDES;
 
-        std::swap(m_direction[UP], m_direction[RIGHT]);
-        std::swap(m_direction[DOWN], m_direction[LEFT]);
-        std::swap(m_direction[UP], m_direction[DOWN]);
-    }
-}
-//=============================================================================
-void Pipe::rotateLeft() {
-
-    if(m_pipeType != SINK)
-        m_sprite.rotate(-DEGREE);
-
-    std::swap(m_direction[UP], m_direction[LEFT]);
-    std::swap(m_direction[DOWN], m_direction[RIGHT]);
-    std::swap(m_direction[UP], m_direction[DOWN]);
+    for(auto i = 0; i < numOfRotate; ++i)
+        rotateLeft();
 }
 //=============================================================================
