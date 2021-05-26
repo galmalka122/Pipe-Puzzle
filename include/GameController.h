dec0381cebac40ccae16880e5aa700d0e8fba6c3
DvirPerkin <Dvirperkin@gmail.com>
2021-05-26 17:11:48 +0300

    Update GameController class
    Victory and level up  screens have been added.
    Sounds were added.
    Now displays the details of the level (level number and clicks made).

--- include/GameController.h
+++ include/GameController.h
@@ -1,13 +1,27 @@
 #pragma once
 
-#include "GameBoard.h"
+#include <cstdlib>
 
+#include "File.h"
+#include "Sound.h"
+#include "Macros.h"
+#include "GameBoard.h"
+#include "StageDetails.h"
+
+// A class that manage the game and controls the interaction between the objects in the game.
 
 class GameController {
 public:
+    GameController();
     void run();
 
 private:
+    File m_stageFile;
     GameBoard m_gameBoard;
+    sf::Sprite m_backGround;
+    StageDetails m_stageDetail;
+
     void draw(sf::RenderWindow &);
+    void nextStage(sf::RenderWindow &);
+    void win(sf::RenderWindow &, const enum Textures_t, const enum Sounds_t, const int);
 };
--- src/GameController.cpp
+++ src/GameController.cpp
@@ -1,23 +1,36 @@
 #include "GameController.h"
-#include "Macros.h"
 
+
+GameController::GameController() : m_stageFile(STAGES_FILE),
+                                    m_backGround(Textures::texturesObject().getSprite(BACKGROUND)){
+
+    m_backGround.setScale(WINDOW_WIDTH / m_backGround.getGlobalBounds().width,
+                          WINDOW_HEIGHT / m_backGround.getGlobalBounds().height);
+}
 //=============================================================================
 void GameController::run() {
     auto window = sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Pipe");
 
-    while (window.isOpen())
-    {
+    // Loads the first stage.
+    nextStage(window);
+
+    while (window.isOpen()){
         window.clear();
-        m_gameBoard.draw(window);
+        draw(window);
         window.display();
 
-        if (auto event = sf::Event{}; window.waitEvent(event))
-        {
-            switch (event.type)
-            {
+        // Loads the next stage.
+        if (m_gameBoard.boardSolved())
+            nextStage(window);
+
+        if (auto event = sf::Event{}; window.waitEvent(event)){
+            switch (event.type){
                 case sf::Event::MouseButtonPressed:
-                    m_gameBoard.nextStage();
-                    m_gameBoard.rotatePipe(event);
+
+                    if(m_gameBoard.rotatePipe(event)) {
+                        m_stageDetail.tap(); // Counts taps.
+                        Sound::soundObject().playSound(CLICK_SOUND);
+                    }
                     break;
 
                 case sf::Event::Closed:
@@ -29,6 +42,57 @@ void GameController::run() {
 }
 //=============================================================================
 void GameController::draw(sf::RenderWindow & window) {
+    window.draw(m_backGround);
+    m_stageDetail.draw(window);
+    m_gameBoard.draw(window);
+}
+//=============================================================================
+void GameController::nextStage(sf::RenderWindow & window) {
+    auto it = m_stageFile.currentIter();
 
+    // Checks if there is no more stages.
+    if(it != m_stageFile.begin()) {
+        if(m_stageFile.eof()) {
+            win(window, WIN_SCREEN, WIN_SOUND, WIN_SCREEN_PAUSE);
+            exit(EXIT_SUCCESS);
+        }
+        else
+            win(window, LEVEL_UP, LEVEL_UP_SOUND, LEVEL_UP_PAUSE);
+    }
+
+    m_stageDetail.initTap();
+    m_stageDetail.increaseStage();
+
+    // Gets the board size to indicate the current stage range.
+    int row, col;
+    auto stream = std::stringstream();
+    stream << m_stageFile.getLine();
+    if(!(stream >> col >> row))
+        throw std::invalid_argument("An issue occurred while reading the game board size from the file");
+
+    m_gameBoard.nextStage(it, it + row * col + 1);
+
+    // Initialize the iterator to the line of the next stage.
+    m_stageFile.next(row * col + 2);
+}
+//=============================================================================
+void GameController::win(sf::RenderWindow & window,
+                         const enum Textures_t enumTextures,
+                         const enum Sounds_t enumSounds,
+                                 int pause) {
+
+    auto winScreen = Textures::texturesObject().getSprite(enumTextures);
+    winScreen.setScale(WINDOW_WIDTH / winScreen.getGlobalBounds().width,
+                       WINDOW_HEIGHT / winScreen.getGlobalBounds().height);
+    window.clear();
+    window.draw(winScreen);
+    window.display();
+
+    Sound::soundObject().playSound(enumSounds);
+
+    sf::sleep(sf::seconds(pause));
+
+    auto event = sf::Event{};
+    while (window.pollEvent(event));
 }
 //=============================================================================
